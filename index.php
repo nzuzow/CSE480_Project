5ccf1a5887b37b1b96d09ba64fad5d06d08035fe
Nick <zuzownic@msu.edu>
2015-02-19 11:14:32 -0500

    First commit. Added an index.php page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// This will be the initial page of the project. This is where a user will 
+// have to login and whatnot.
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>CSE 480 Project</title>
+</head>
+<body>
+	<p>Hello World!</p>
+</body>
+</html>
